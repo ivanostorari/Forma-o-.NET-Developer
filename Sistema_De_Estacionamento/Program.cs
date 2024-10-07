59352ca6265852981cae952073cb681a415d03a9
ivanostorari <programadordev41@gmail.com>
2024-10-07 16:30:19 -0400

    Sistema de Estacionamento

--- Sistema_De_Estacionamento/Program.cs
+++ Sistema_De_Estacionamento/Program.cs
@@ -1,10 +1,60 @@
-﻿namespace Sistema_de_Estacionamento
+﻿using System;
+using Sistema_de_Estacionamento.Models;
+
+// Coloca o encoding para UTF8 para exibir acentuação
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+decimal precoInicial = 0;
+decimal precoPorHora = 0;
+
+Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
+                  "Digite o preço inicial:");
+precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Agora digite o preço por hora:");
+precoPorHora = Convert.ToDecimal(Console.ReadLine());
+
+// Instancia a classe Estacionamento, já com os valores obtidos anteriormente
+Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
+
+string opcao = string.Empty;
+bool exibirMenu = true;
+
+// Realiza o loop do menu
+while (exibirMenu)
 {
-    internal class Program
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("1 - Cadastrar veículo");
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Listar veículos");
+    Console.WriteLine("4 - Encerrar");
+
+    switch (Console.ReadLine())
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        case "1":
+            es.AdicionarVeiculo();
+            break;
+
+        case "2":
+            es.RemoverVeiculo();
+            break;
+
+        case "3":
+            es.ListarVeiculos();
+            break;
+
+        case "4":
+            exibirMenu = false;
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
     }
+
+Console.WriteLine("Pressione uma tecla para continuar");
+Console.ReadLine();
 }
+
+Console.WriteLine("O programa se encerrou");
--- Sistema_De_Estacionamento/obj/Debug/net6.0/Sistema_de_Estacionamento.AssemblyInfo.cs
+++ Sistema_De_Estacionamento/obj/Debug/net6.0/Sistema_de_Estacionamento.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sistema_de_Estacionamento")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c10e35be43529631f8fc6cf8566f2cec2aa3bc3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f15454ad0cc2e4b1795a9153e8caa343b483a829")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sistema_de_Estacionamento")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sistema_de_Estacionamento")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
